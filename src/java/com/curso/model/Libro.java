d265a254863397ab7789fb3659d5a4c00f4e78bf
Daniel Fuentes <giracros@me.com>
2015-07-01 18:11:57 -0500

    Version 6
    Version 6

--- src/java/com/curso/model/Libro.java
+++ src/java/com/curso/model/Libro.java
@@ -4,6 +4,10 @@ import java.util.List;
 
 import com.curso.database.DataBaseHelper;
 
+/**
+ *
+ * @author Daniel Fuentes
+ */
 public class Libro {
 
     private String isbn;
@@ -34,27 +38,60 @@ public class Libro {
         this.categoria = categoria;
     }
 
+    /**
+     *
+     * @return
+     */
     public int insertar() {
         return ejecutarOperacion(String.format("insert into libros (isbn, titulo, categoria) values ('%s', '%s', '%s')", isbn, titulo, categoria));
     }
 
+    /**
+     *
+     * @return
+     */
     public int modificar() {
         return ejecutarOperacion(String.format("update libros set titulo = '%s', categoria = '%s' where isbn = '%s'", titulo, categoria, isbn));
     }
 
+    /**
+     *
+     * @return
+     */
     public int eliminar() {
         return ejecutarOperacion(String.format("delete from libros where isbn = '%s'", isbn));
     }
 
+    /**
+     *
+     * @param categoria
+     * @return
+     */
+    public static List<Libro> findLibrosByCategoria(String categoria) {
+        return ejecutarConsulta("select * from libros where categoria = '" + categoria + "'", Libro.class);
+    }
+
+    /**
+     *
+     * @return
+     */
     public Libro findByIsbn() {
-        List<Libro> list = ejecutarConsulta(String.format("select isbn,titulo,categoria from libros where isbn = '%s'", isbn),Libro.class);
+        List<Libro> list = ejecutarConsulta(String.format("select isbn,titulo,categoria from libros where isbn = '%s'", isbn), Libro.class);
         return list.get(0);
     }
-    
+
+    /**
+     *
+     * @return
+     */
     public static List<Libro> listar() {
         return ejecutarConsulta("select isbn, titulo, categoria from libros", Libro.class);
     }
 
+    /**
+     *
+     * @return
+     */
     public static List<String> listarCategorias() {
         return ejecutarConsulta("select distinct categoria from libros", String.class);
     }
